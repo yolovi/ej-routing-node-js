54087eb5411ee4b2b5e2175856e6aad9b83e501b
yolovi <yolandalopevi@gmail.com>
2023-06-27 17:28:36 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// Crea código HTML para servir una página web clásica (Home| ¿Quiénes somos? | ¿Dónde Estamos? | ¿Qué hacemos? | Contacto... etc...)
+// Ahora sirve respuesta a la petición de cada ruta (endpoint) los ficheros HTML correspondientes: home.html, contact.html, about.html...
+// Realiza las modificaciones necesarias para que nuestro script se ejecute con npm start.
+
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+
+http.createServer((req, res) => {
+const q = url.parse(req.url, true);
+const filename = "." + q.pathname;
+fs.readFile(filename, (error, data) => {
+try {
+res.writeHead(200, { "Content-Type": "text/html" });
+res.write(data);
+return res.end();
+} catch (error) {
+res.writeHead(404, { "Content-Type": "text/html" });
+console.error(error);
+return res.end("404 Not Found");
+}
+});
+}).listen(8080);
